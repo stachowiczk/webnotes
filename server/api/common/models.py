03e4939c121926bd59ae9624f1941f01f1ada7d8
Karol Stachowicz <kstachowicz@icloud.com>
2023-03-03 14:43:07 +0100

    add uuid primary keys and apply relevant migrations

--- server/api/common/models.py
+++ server/api/common/models.py
@@ -1,19 +1,22 @@
 from datetime import datetime
 from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Text
+import uuid
 from sqlalchemy.orm import relationship
 from api.db import db
 
 
 class Note(db.Model):
     __tablename__ = "notes"
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.String(36), primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text, nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     folder_id = db.Column(db.Integer, db.ForeignKey("folders.id"), nullable=True)
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
-    
 
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.id = str(uuid.uuid4())
 
     def __repr__(self):
         return f"Note id: {self.id}, title: {self.title}"
