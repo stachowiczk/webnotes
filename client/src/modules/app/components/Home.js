42337a385faaa650725ea0f3f059779207565c91
Karol Stachowicz <kstachowicz@icloud.com>
2023-03-27 12:06:02 +0200

    update imports for Menu.js

--- client/src/modules/app/components/Home.js
+++ client/src/modules/app/components/Home.js
@@ -4,7 +4,7 @@ import Draggable from "react-draggable";
 import EditorComponent from "../../common/components/Editor.js";
 import http from "../../auth/components/Interceptor";
 import TextFeed from "../../common/components/TextFeed.js";
-import Menu from "./Menu.js";
+import Menu from "../../auth/components/Menu.js";
 import { setReload } from "../../common/slices/feedSlice.js";
 import {
   setEditedNoteId,
--- client/src/modules/auth/components/Menu.js
+++ client/src/modules/auth/components/Menu.js
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { AuthContext } from "../../auth/context/UserContext";
+import { AuthContext } from "../context/UserContext";
 
 function Menu() {
   //eslint-disable-next-line
